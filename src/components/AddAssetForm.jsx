aee3a21da42813b199c220a01f4deac347c69a1a
Alexander <shvitc@gmail.com>
2024-10-23 10:55:23 +0300

    Пишем криптовалюту

--- src/components/AddAssetForm.jsx
+++ src/components/AddAssetForm.jsx
@@ -1,3 +1,43 @@
+import {useState} from "react";
+import {Select, Space, Typography, Flex} from "antd";
+
+import {useCrypto} from "../context/crypto-context";
+
 export default function AddAssetForm() {
-	return <form>FORM ASSET</form>;
+	const [coin, setCoin] = useState(null);
+	const {crypto} = useCrypto();
+
+	if (!coin) {
+		return (
+			<Select
+				style={{width: "100%"}}
+				onSelect={(value) => setCoin(crypto.find((c) => c.id === value))}
+				placeholder='Select coin'
+				options={crypto.map((coin) => ({
+					label: coin.name,
+					value: coin.id,
+					icon: coin.icon,
+				}))}
+				optionRender={(option) => (
+					<Space>
+						<img src={option.data.icon} alt={option.data.label} style={{width: 20}} /> {option.data.label}
+					</Space>
+				)}
+			/>
+		);
+	}
+
+	return (
+		<form>
+			<Flex alignItems={"center"} gap={10}>
+				<img src={coin.icon} alt={coin.name} style={{width: 40}} />
+				<Typography.Title level={2} style={{margin: 0}}>
+					({coin.symbol}) {coin.name}
+				</Typography.Title>
+			</Flex>
+			<Typography.Title level={2} style={{margin: 0}}>
+				{coin.name}
+			</Typography.Title>
+		</form>
+	);
 }
--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -18,7 +18,7 @@ export default function AppHeader() {
 	const [select, setSelect] = useState(false);
 	const [coin, setCoin] = useState(null);
 	const [modal, setModal] = useState(false);
-	const [drawer, setDrawer] = useState(false);
+	const [drawer, setDrawer] = useState(true);
 	const {crypto} = useCrypto();
 
 	useEffect(() => {
@@ -66,7 +66,7 @@ export default function AppHeader() {
 				<CoinInfoModal coin={coin} />
 			</Modal>
 
-			<Drawer width={600} title='Add Asset' onClose={() => setDrawer(false)} open={drawer}>
+			<Drawer width={600} title='Add Asset' onClose={() => setDrawer(false)} open={drawer} destroyOnClose>
 				<AddAssetForm />
 			</Drawer>
 		</Layout.Header>
